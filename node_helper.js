94b94181f3096b6c7e6370587bfac6ed471aacfe
Andreas Jocham <github@designstube.de>
2024-11-21 09:21:15 +0100

    Add files via upload

--- node_helper.js
+++ node_helper.js
@@ -3,54 +3,40 @@ const axios = require("axios");
 
 module.exports = NodeHelper.create({
   start() {
-    console.log("Starting node helper for MMM-RoomData...");
+    console.log("Starting node helper for MMM-ioBrokerWaschmaschine...");
   },
 
   socketNotificationReceived(notification, payload) {
-    if (notification === "GET_ROOM_DATA") {
+    if (notification === "GET_WASHER_DATA") {
       this.fetchRoomData(payload);
     }
   },
 
   async fetchRoomData(config) {
-    const roomData = [];
+    const washerData = [];
     const baseURL = `http://${config.ip}:${config.port}/get`;
 
-    //http://192.168.4.211:8082/get/hmip.0.groups.4846376a-291c-445c-ac70-51d3f6860b5b.windowOpen/ts
+    //http://192.168.4.211:8082/get/homeconnect.0.BOSCH-WAV28E43-68A40E9452D3.programs.active.options.BSH_Common_Option_ProgramProgress
 
 
-    for (const room of config.rooms) {
+    for (const washer of config.washers) {
       try {
-        const [temperature, humidity, windowStatus, windowStatusMeta, outsideTemperature] = await Promise.all([
-          axios.get(`${baseURL}/${room.temperatureID}`),
-          axios.get(`${baseURL}/${room.humidityID}`),
-          axios.get(`${baseURL}/${room.windowStatusID}`),
-          axios.get(`${baseURL}/${room.windowStatusID}/lc`),  // Metadata for last change timestamp
-          axios.get(`${baseURL}/${room.outsideTemperatureID}`)
+        const [progress, remainingTime] = await Promise.all([
+          axios.get(`${baseURL}/${washer.progressID}`),
+          axios.get(`${baseURL}/${washer.remainingTimeID}`)
         ]);
 
-        // Calculate open duration if the window is open
-        let openDuration = 0;
-        if (windowStatus.data.val) { // Window is open
-          const lastChanged = windowStatusMeta.data.lc;
-          openDuration = Math.floor((Date.now() - lastChanged) / 60000); // Duration in seconds
-        }
-
-        roomData.push({
-          name: room.name,
-          temperature: temperature.data.val,
-          humidity: humidity.data.val,
-          windowStatus: windowStatus.data.val,
-          outsideTemperature: outsideTemperature.data.val,
-          lastChanged: windowStatusMeta.data.ts,
-          openDuration: openDuration
+         washerData.push({
+          name: washer.name,
+          progress: progress.data.val,
+          remainingTime: remainingTime.data.val
         });
       } catch (error) {
-        console.error("Error fetching data for room:", room.name, error);
+        console.error("Error fetching data for washer:", washer.name, error);
       }
     }
 
-    this.sendSocketNotification("ROOM_DATA_RESULT", roomData);
+    this.sendSocketNotification("WASHER_DATA_RESULT", washerData);
   }
 });
 
@@ -161,3 +147,5 @@ module.exports = NodeHelper.create({
   }
 });
 */
+
+
